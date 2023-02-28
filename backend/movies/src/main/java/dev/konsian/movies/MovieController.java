3793cb431a2ba6906438d60cac5297470da34403
konsian <40067358+konsian@users.noreply.github.com>
2023-03-01 00:52:52 +0300

    Update MovieController.java

--- backend/movies/src/main/java/dev/konsian/movies/MovieController.java
+++ backend/movies/src/main/java/dev/konsian/movies/MovieController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
     @Autowired
     private MovieService movieService;
